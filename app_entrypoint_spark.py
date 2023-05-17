9831cd2c79e07c1810ffb367aa3c28dc0695c7d6
swethabikkasani <43268501+swethabikkasani@users.noreply.github.com>
2023-05-17 16:30:21 +0530

    Create app_entrypoint_spark.py

---
+++ app_entrypoint_spark.py
@@ -0,0 +1,10 @@
+from src.main_app.start_app import start_app
+import argparse
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--app_name", help="nono")
+
+    args = parser.parse_args()
+
+    start_app(args.app_name)
