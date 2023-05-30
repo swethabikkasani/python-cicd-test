f00c5b27bd1b7ea0421811713fd941a64f1ae3c5
swethabikkasani <43268501+swethabikkasani@users.noreply.github.com>
2023-05-30 13:23:25 +0530

    Create tests_basic.py

---
+++ tests/tests_basic.py
@@ -0,0 +1,40 @@
+import unittest
+
+from src_spark_app.main_app.calculations import Calculations
+from src_spark_app.main_app.prime_no import is_prime
+
+
+class Testing(unittest.TestCase):
+    def test_string(self):
+        a = 'some'
+        b = 'some'
+        self.assertEqual(a, b)
+
+    def test_boolean(self):
+        a = True
+        b = True
+        self.assertEqual(a, b)
+
+    def test_prime(self):
+        a = 3
+        self.assertTrue(is_prime(a))
+
+    def test_sum(self):
+        calculation = Calculations(8, 2)
+        self.assertEqual(calculation.get_sum(), 10, 'The sum is wrong.')
+
+    def test_sum(self):
+        calculation = Calculations(8, 2)
+        self.assertEqual(calculation.get_sum(), 10, 'The sum is wrong.')
+
+    def test_diff(self):
+        calculation = Calculations(8, 2)
+        self.assertEqual(calculation.get_difference(), 6, 'The difference is wrong.')
+
+    def test_product(self):
+        calculation = Calculations(8, 2)
+        self.assertEqual(calculation.get_product(), 16, 'The product is wrong.')
+
+    def test_quotient(self):
+        calculation = Calculations(8, 2)
+        self.assertEqual(calculation.get_quotient(), 4, 'The quotient is wrong.')
